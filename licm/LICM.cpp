0cb930e9448a871fe58b49bdacb82aa021afd8da
andja <spasic.andjela2013@gmail.com>
2026-05-14 02:41:25 +0200

    feat(licm): add pass statistics and debug logging

--- licm/LICM.cpp
+++ licm/LICM.cpp
@@ -1,14 +1,24 @@
+#include "llvm/ADT/Statistic.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Transforms/Scalar/LoopPassManager.h"
 
+#define DEBUG_TYPE "licm"
+
 using namespace llvm;
+
+STATISTIC(NumHoisted, "Number of instructions hoisted out of loops");
+STATISTIC(NumSunk, "Number of instructions sunk out of loops");
+STATISTIC(NumPromoted, "Number of memory locations promoted to registers");
 
 struct LICMPass : PassInfoMixin<LICMPass> {
     PreservedAnalyses run(Loop &L, LoopAnalysisManager &LAM,
                           LoopStandardAnalysisResults &AR, LPMUpdater &U) {
+        LLVM_DEBUG(dbgs() << "[licm] running on loop: "
+                          << L.getName() << "\n");
         return PreservedAnalyses::all();
     }
 };
