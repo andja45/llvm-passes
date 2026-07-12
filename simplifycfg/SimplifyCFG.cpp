0c0d71fce00d69cc120439ba2f528eb554ec7481
Ana Stevanovic <anas2811@gmail.com>
2026-07-12 14:15:52 +0200

    feat(simplifycfg): implement basic block merging

--- simplifycfg/SimplifyCFG.cpp
+++ simplifycfg/SimplifyCFG.cpp
@@ -2,6 +2,7 @@
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/CFG.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -11,8 +12,13 @@ using namespace llvm;
 
 namespace {
 
+static bool canMergeBlocks(BasicBlock &BB, BasicBlock *&Pred);
+static void simplifyBlockPHIs(BasicBlock &BB);
+static void updateSuccessorPHIs(BasicBlock &BB, BasicBlock &Pred);
+static void moveInstructions(BasicBlock &From, BasicBlock &To);
+static void mergeBlocks(BasicBlock &BB, BasicBlock &Pred);
+
 static bool removeUnreachableBlocks(Function &F) {
-    
     if (F.empty())
         return false;
 
@@ -57,17 +63,89 @@ static bool removeUnreachableBlocks(Function &F) {
     return true;
 }
 
+static bool mergeBasicBlocks(Function &F) {
+    bool Changed = false;
+    bool LocalChange = true;
+
+    while (LocalChange) {
+        LocalChange = false;
+
+        for (BasicBlock &BB : F) {
+            BasicBlock *Pred = nullptr;
+
+            if (!canMergeBlocks(BB, Pred))
+                continue;
+
+            mergeBlocks(BB, *Pred);
+
+            Changed = true;
+            LocalChange = true;
+            break;
+        }
+    }
+
+    return Changed;
+}
+
 struct SimplifyCFGPass : PassInfoMixin<SimplifyCFGPass> {
 
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+        bool Changed = false;
+        Changed |= removeUnreachableBlocks(F);
+        Changed |= mergeBasicBlocks(F);
 
-        bool Changed = removeUnreachableBlocks(F);
         if (Changed)
             return PreservedAnalyses::none();
-
+    
         return PreservedAnalyses::all();
     }
 };
+
+static bool canMergeBlocks(BasicBlock &BB, BasicBlock *&Pred) {
+    Pred = BB.getSinglePredecessor();
+
+    if (!Pred || Pred == &BB)
+        return false;
+
+    auto *Branch = dyn_cast<BranchInst>(Pred->getTerminator());
+    if (!Branch || !Branch->isUnconditional())
+        return false;
+
+    return Branch->getSuccessor(0) == &BB;
+}
+
+static void mergeBlocks(BasicBlock &BB, BasicBlock &Pred) {
+    simplifyBlockPHIs(BB);
+    updateSuccessorPHIs(BB, Pred);
+
+    Pred.getTerminator()->eraseFromParent();
+
+    moveInstructions(BB, Pred);
+    
+    BB.eraseFromParent();
+}
+
+static void simplifyBlockPHIs(BasicBlock &BB) {
+    while (auto *Phi = dyn_cast<PHINode>(&BB.front())) {
+        Value *IncomingValue = Phi->getIncomingValue(0);
+        Phi->replaceAllUsesWith(IncomingValue);
+        Phi->eraseFromParent();
+    }
+}
+
+static void updateSuccessorPHIs(BasicBlock &BB, BasicBlock &Pred) {
+    for (BasicBlock *Successor : successors(&BB)) {
+        for (PHINode &Phi : Successor->phis())
+            Phi.replaceIncomingBlockWith(&BB, &Pred);
+    }
+}
+
+static void moveInstructions(BasicBlock &From, BasicBlock &To) {
+    while (!From.empty()) {
+        Instruction &I = From.front();
+        I.moveBefore(To, To.end());
+    }
+}
 
 }
 
