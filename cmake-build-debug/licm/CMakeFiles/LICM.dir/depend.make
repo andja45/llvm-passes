dc535b2822a1f67cf36b08f7d4b34e0d0bfe4b3b
Dunja Milenkovic <dunja.milenkovic5@gmail.com>
2026-07-13 15:32:16 +0200

    chore(jump-threading): remove and ignore generated files

--- cmake-build-debug/licm/CMakeFiles/LICM.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LICM.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/licm/CMakeFiles/install-LICM.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LICM"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-LICM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
